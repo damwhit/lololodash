f05cfced8adfecbb97981abfda4467a5f4317e0d
David Whitaker <damwhit@gmail.com>
2016-05-06 09:43:51 -0600

    first exercise passing

--- filter.js
+++ filter.js
@@ -1,11 +1,7 @@
 var _ = require("lodash")
 
-var users = [
-  { id: 22, username: "martin", active: true},
-  { id: 23, username: "max",    active: false},
-  { id: 24, username: "linda",  active: false}
-]
-
-var worker = function(users) {
-  _.filter(active: true)
+var filterwhere = function(users) {
+  return _.filter(users, {active: true})
 }
+
+module.exports = filterwhere;
